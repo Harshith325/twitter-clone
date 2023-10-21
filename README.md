c73701ca0bf1cbb6bce6da8f4b08acc973d30622
Harshith325 <harshithkothari5@gmail.com>
2023-10-21 20:11:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# twitter-clone
+
+Fully functional mern-stack twitter clone application
+
+## 💡 Features
+- Auth (Login/Register)
+- Upload Profile Picture/Banner (With resize)
+- Create tweet
+- Follow/Unfollow
+- Change user information
+- Fully responsive design
+
+## 👨‍💻 Used technologies
+`React`, `Redux`, `Tailwind`, `Axios`, `Express`, `Mongodb`, `Multer`, `Sharp`, `JWT` 
+ 
---
+++ client/src/components/suggestions/Hashtags.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import More from '../../assets/more.svg'
+
+function App() {
+
+    const [tags, setTags] = useState([
+        "Harshith is God",
+        "Real World",
+        "#riseforjustice",
+        "#protest",
+        "Stock Picks",
+        "#Lorem",
+        "#Ipsum"
+    ])
+
+    return (
+        <div id='hashtags' className='w-full bg-[#16181C] rounded-2xl pt-3' >
+            <span className='font-bold text-xl px-4 pb-2' >Topics that may interest you</span>
+            <div className='h-[12px] ' ></div>
+
+            {
+                tags.map((item, index) => {
+                    return (
+                        <div key={index} className='flex items-start flex-col px-4 py-2 w-full cursor-pointer transition-all hover:bg-[#1D1F23] ' >
+                            <div className='flex items-start flex-col w-full' >
+                                <div className='flex justify-between items-center w-full' >
+                                    <span className='text-sm text-[#54595D]' >Politics - Trending</span>
+                                    <img src={More} width="20" alt="" />
+                                </div>
+                                <span className="font-semibold" > {item} </span>
+                            </div>
+                            <span className='text-sm text-[#54595D] ' > {Math.floor(Math.random() * (10000 - 1500) + 1500)} Tweet</span>
+                        </div>
+                    )
+                })
+            }
+
+            <div className='flex items-start flex-col px-4 py-3 mt-2 w-full cursor-pointer transition-all hover:bg-[#1D1F23] rounded-b-2xl' >
+                <span className='text-[#1d9bf0] ' >Show more</span>
+            </div>
+
+        </div>
+    )
+}
+
+export default App
